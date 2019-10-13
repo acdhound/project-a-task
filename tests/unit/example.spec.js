475771b739677140aafbe0a168794b09db37c75a
acdhound <adivanov95@mail.ru>
2019-10-13 20:37:19 +0300

    test negotiation panel

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,18 @@ import { shallowMount } from '@vue/test-utils'
 import NegotiationPanel from '@/components/NegotiationPanel.vue'
 
 describe('NegotiationPanel.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(NegotiationPanel, {
-      propsData: { }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+  it('success title if the offer is bigger', () => {
+    const wrapper = shallowMount(NegotiationPanel, { propsData: { } });
+    wrapper.vm.changeEmployeeValue(10.0);
+    wrapper.vm.changeEmployerValue(12.0);
+    wrapper.vm.validateAndShowModal();
+    expect(wrapper.vm.modalTitle).toBe('Success!');
+  });
+  it('failure title if the offer is smaller', () => {
+    const wrapper = shallowMount(NegotiationPanel, { propsData: { } });
+    wrapper.vm.changeEmployeeValue(15.0);
+    wrapper.vm.changeEmployerValue(12.0);
+    wrapper.vm.validateAndShowModal();
+    expect(wrapper.vm.modalTitle).toBe('Failure!');
+  });
+});
