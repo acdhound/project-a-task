273042f531cbb642d9e5b6e9c7d7583ddea0ffa8
acdhound <adivanov95@mail.ru>
2019-10-13 22:33:49 +0300

    test showing and hiding the modal window

--- tests/unit/NegotiationPanel.spec.js
+++ tests/unit/NegotiationPanel.spec.js
@@ -1,5 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import NegotiationPanel from '@/components/NegotiationPanel.vue'
+import ModalDialog from '@/components/ModalDialog.vue'
 
 describe('NegotiationPanel.vue', () => {
   it('success title if the offer is bigger', () => {
@@ -15,5 +16,22 @@ describe('NegotiationPanel.vue', () => {
     wrapper.vm.changeEmployerValue(12.0);
     wrapper.vm.validateAndShowModal();
     expect(wrapper.vm.modalTitle).toBe('Failure!');
+  });
+  it('modal window is shown after submitting the values', () => {
+    const wrapper = mount(NegotiationPanel, { propsData: { } });
+    const modalWrapper = wrapper.find(ModalDialog);
+    expect(modalWrapper.isVisible()).toBe(false);
+    wrapper.vm.changeEmployeeValue(1);
+    wrapper.vm.changeEmployerValue(2);
+    wrapper.vm.validateAndShowModal();
+    expect(modalWrapper.isVisible()).toBe(true);
+  });
+  it('modal window disappears after pressing Ok button', () => {
+    const wrapper = mount(NegotiationPanel, { propsData: { } });
+    const modalWrapper = wrapper.find(ModalDialog);
+    wrapper.vm.modalActive = true;
+    expect(modalWrapper.isVisible()).toBe(true);
+    modalWrapper.vm.$emit('press-ok', {});
+    expect(modalWrapper.isVisible()).toBe(false);
   });
 });
